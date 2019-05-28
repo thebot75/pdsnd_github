c8ffa38dfefcca1639d6bd86591ac9aa505e9fd3
Scott Young <scott.young@rbs.co.uk>
2019-05-28 17:19:07 +0100

    Tidied up print command on row 155 of python code

--- bikeshare.py
+++ bikeshare.py
@@ -20,16 +20,16 @@ def get_filters():
     valid_days = ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     #these are the valid options for the three variables that allow us to continue with the rest of the code
     #anything else will cause us to loop round until we have one valid variable in each field
-    
+
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = ""
     city_names = { 'ch': 'chicago',
                   'ny': 'new york city',
                   'wa': 'washington' }
-    #note: different to valid list - used dictionary so that user has to type less to continue (and 
+    #note: different to valid list - used dictionary so that user has to type less to continue (and
     #less chance of mistakes by doing it this way)
-    
+
     while city not in valid_cities:
         city_short = input("Please enter a city (For Chicago enter 'ch' / New York City enter 'ny'/ Washington enter 'wa': ").lower()
         if city_short not in city_names:
@@ -38,7 +38,7 @@ def get_filters():
             city = city_names[city_short]
         #this just loops around until we get a valid input - city_names dictionary used to derive 'proper' name
         #from shorter user input
-            
+
     # get user input for month (all, january, february, ... , june)
     month = ""
     month_names = { 'jan': 'january',
@@ -48,9 +48,9 @@ def get_filters():
                   'may': 'may',
                   'jun': 'june',
                   'all': 'all'}
-    #note: different to valid list - used dictionary so that user has to type less to continue (and 
+    #note: different to valid list - used dictionary so that user has to type less to continue (and
     #less chance of mistakes by doing it this way)
-    
+
     while month not in valid_months:
         month_short = input("Please enter a month (jan / feb / mar / apr / may/ jun), or enter 'all' to see all data: ").lower()
         if month_short not in month_names:
@@ -70,9 +70,9 @@ def get_filters():
                  'sat': 'saturday',
                  'sun': 'sunday',
                  'all': 'all'}
-    #note: different to valid list - used dictionary so that user has to type less to continue (and 
+    #note: different to valid list - used dictionary so that user has to type less to continue (and
     #less chance of mistakes by doing it this way)
-    
+
     while day not in valid_days:
         day_short = input("Please enter a day (mon / tue / wed / thu / fri / sat / sun), or enter 'all' to see all data: ").lower()
         if day_short not in day_names:
@@ -131,28 +131,28 @@ def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()   
- 
+    start_time = time.time()
+
     # display the most common month
     df['month'] = df['Start Time'].dt.month
     popular_month = df['month'].mode()[0]
     months = ['january', 'february', 'march', 'april', 'may', 'june']
     month = months[popular_month - 1]
     print('The most common start month for a journey is ' + str(month).title() + ".")
-    
+
     # display the most common day of week
     popular_day = df['weekday'].mode()[0]
-    
+
     print('The most common weekday for a journey is ' + str(popular_day) + ".")
 
     # display the most common start hour
     # extract hour from the Start Time column to create an hour column
     df['hour'] = df['Start Time'].dt.hour
-    
+
     # find the most common hour (from 0 to 23)
     popular_hour = df['hour'].mode()[0]
     if popular_hour < 13:
-        print('The most common hour for starting a journey is ' + str(popular_hour) + "AM.")
+        print('The most common hour for starting a journey is {}AM.'.format(popular_hour))
     else:
         print('The most common hour for starting a journey is ' + str(popular_hour - 12) + "PM.")
     #this section just converts the raw hour format into something a bit more readable for the user
@@ -166,36 +166,36 @@ def station_stats(df):
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
-    
+
     df['total_trip'] = df['Start Station'] + " to " + df['End Station']
     #this sets up a new column for total trip, which is a combination of start and end station
-    
+
     total_rows = len(df['Start Station'])
-    
+
     # display most commonly used start station
     most_starts = df['Start Station'].mode()
     maxstarts = df['Start Station'].value_counts().max()
-    #these two lines derive the most commonly used start station using mode, and the number of times that 
+    #these two lines derive the most commonly used start station using mode, and the number of times that
     #station was used using value_counts
-    
+
     print("The most common start station was " + most_starts[0] + ", which was the start station \n" +                 str(maxstarts) + " times out of " + str(total_rows) + " total journeys.\n")
 
     # display most commonly used end station
     most_ends = df['End Station'].mode()
     maxends = df['End Station'].value_counts().max()
-    
-    #these two lines derive the most commonly used end station using mode, and the number of times that 
+
+    #these two lines derive the most commonly used end station using mode, and the number of times that
     #station was used using value_counts
-    
-    print("The most common end station was " + most_ends[0] + ", which was the final station \n" + str(maxends) + 
+
+    print("The most common end station was " + most_ends[0] + ", which was the final station \n" + str(maxends) +
     " times out of " + str(total_rows) + " total journeys.\n")
 
     # display most frequent combination of start station and end station trip
     most_trips = df['total_trip'].mode()
     maxtotal = df['total_trip'].value_counts().max()
    #these two lines use the 'total_trip' series we created above, and use this to derive the most common
-   #trip using mode, and the number of times that trip was taken using value_counts 
-    
+   #trip using mode, and the number of times that trip was taken using value_counts
+
     print("The most common trip is from " + most_trips[0] + ", which\nwas travelled "
     + str(maxtotal) + " times out of " + str(total_rows) + " total journeys.")
 
@@ -213,15 +213,15 @@ def trip_duration_stats(df):
     total_duration = df['Trip Duration'].sum()
     print("The total time spent travelling across all customers was " + str(int(round(total_duration/360)))
     + " hours.")
-        
+
     # display mean travel time
     mean_duration = df['Trip Duration'].mean()
     print("The average travel time was " + str(int(round(mean_duration/60))) + " minutes.")
 
-    #the above use sum and mean to get the stats for the travel time in seconds, convert that to  
+    #the above use sum and mean to get the stats for the travel time in seconds, convert that to
     #hours for total / minutes for mean, then uses round & int to get to the nearest integer (& str
     #so it prints)
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -231,17 +231,17 @@ def user_stats(df, city):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    
-    
+
+
     # Display counts of user types
     print("We had the following types of users in our customer base over this period: \n")
     userdf = df['User Type'].value_counts().reset_index()
     print(userdf.to_string(header=None, index=None))
     print("\n")
-    #this convert to another df, then from that df to_string, is a neat method of getting rid of 
+    #this convert to another df, then from that df to_string, is a neat method of getting rid of
     #the additional info (dtype etc) that you get using value_counts
-    
-    
+
+
     if city == 'washington':
         print("We have no gender or year of birth data for our Washington customer base, sorry.")
         #this makes sure the code doesn't fall over if the city is Washington, using city from get_filters()
@@ -251,7 +251,7 @@ def user_stats(df, city):
         genderdf = df['Gender'].value_counts().reset_index()
         print(genderdf.to_string(header=None, index=None))
         print("\n")
-        #this convert to another df, then from that df to_string, is a neat method of getting rid of 
+        #this convert to another df, then from that df to_string, is a neat method of getting rid of
         #the additional info (dtype etc) that you get using value_counts
 
         # Display earliest, most recent, and most common year of birth
@@ -264,7 +264,7 @@ def user_stats(df, city):
 
 def raw_data(df):
     """Asks if users want to see the raw dataframe data, then lets them see it 5 rows at a time"""
-    
+
     display_data = input("\nWould you like to see the raw data? (yes to see data, no to continue): ").lower()
     if display_data == 'yes':
         start_data = 0
@@ -282,9 +282,9 @@ def raw_data(df):
                 end_data = min(end_data + 5, len(df['Start Station']))
                 cont_data = input("\nDo you want to see another 5 rows? (enter yes to continue, no to quit:                         ").lower()
         #this while loop keeps going until user enters 'not yes', or until we reach the end of the data
-        #on each run round the loop start data is increased by 5, as is end data UNLESS that would take it past 
+        #on each run round the loop start data is increased by 5, as is end data UNLESS that would take it past
         #the final piece of data - in which case it's limited to the final index
-    
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -295,7 +295,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df, city)
         raw_data(df)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
